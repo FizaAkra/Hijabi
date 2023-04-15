1e9ce4a807648d9acea51fa5e0e9d49c7544d1c0
fizaakram56@gmail.com <fizaakram56@gmail.com>
2023-04-15 17:40:24 +0500

    Add project files.

---
+++ Hijab/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hijab.MvcApplication" Language="C#" %>
---
+++ Hijab/Models/ShopModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hijab.Models
+{
+    public class ShopModel
+    {
+        public List<Category> cat { get; set; }
+        public List<Product> Pro { get; set; }
+    }
+}
